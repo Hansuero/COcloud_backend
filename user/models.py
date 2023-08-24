214a74d2b0ff9dd259b03721cdb612aaad4e2d68
nekovoo <1398993569@qq.com>
2023-08-24 15:45:55 +0800

    新增用户，登录，注册“

---
+++ user/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class User(models.Model):
+    ROLES = (
+        ('creator', '团队创建者'),
+        ('admin', '团队管理员'),
+        ('member', '普通成员'),
+    )
+
+    username = models.CharField('用户名', max_length=16)
+    password = models.CharField('密码', max_length=20)
+    email = models.EmailField('邮箱', unique=True)
+    role = models.CharField('职位', max_length=10, choices=ROLES)
+    created_at = models.DateTimeField('创建日期', auto_now_add=True)
+    photo_url = models.CharField('用户头像路径', max_length=128, default='')
+
+    def __str__(self):
+        return self.username
+# Create your models here.
---
+++ user/views.py
@@ -0,0 +1,53 @@
+import os
+
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from COcloud_backend.settings import BASE_DIR
+from user.models import User
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        if User.objects.filter(username=username).exists():
+            result = {'result': 2, 'message': r'用户已存在！'}
+            return JsonResponse(result)
+
+
+        email = request.POST.get('email', '')
+        photo_url = os.path.join(BASE_DIR, 'photo', 'default.jpg')
+
+        user = User.objects.create(username=username, password=password, email=email, photo_url=photo_url)
+        user.save()
+        request.session['username'] = username
+        request.session.set_expiry(3600)
+        result = {'result': 0, 'message': r'注册成功！'}
+        return JsonResponse(result)
+    else:
+        result = {'result': 1, 'message': r"请求方式错误！"}
+        return JsonResponse(result)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not User.objects.filter(username=username):
+            result = {'result': 2, 'message': r'查无此人！'}
+            return JsonResponse(result)
+        user = User.objects.get(username=username)
+        if user.password == password:
+            request.session['username'] = username
+            request.session.set_expiry(3600)
+            result = {'result': 0, 'message': r'登录成功！'}
+            return JsonResponse(result)
+        else:
+            result = {'result': 3, 'message': r'密码错误！'}
+            return JsonResponse(result)
+    else:
+        result = {'result': 1, 'message': r'请求方式错误！'}
+        return JsonResponse(result)
+# Create your views here.
