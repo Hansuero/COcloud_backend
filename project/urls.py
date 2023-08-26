cd6a56f1ec0a9455ac7f874d4734e3dae8e1f8c3
nekovoo <1398993569@qq.com>
2023-08-26 17:02:56 +0800

    新增对项目的控制

---
+++ project/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from project.views import *
+
+urlpatterns = [
+    path('create_project', create_project),
+    path('rename_project', rename_project),
+    path('delete_project', delete_project),
+    path('get_project', get_project),
+
+]
