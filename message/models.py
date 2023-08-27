147b2b1dd052ca7c41d2493cc057049357378b73
Hansuero <93417959+Hansuero@users.noreply.github.com>
2023-08-27 20:03:16 +0800

    修改数据库Report模型，删除了Report部分外键。

--- message/models.py
+++ message/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 
 from chat.models import GroupChatMessage
+from team.models import Team
 from user.models import User
 
 
 # Create your models here.
 class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    chat_message = models.ForeignKey(GroupChatMessage, blank=True, null=True, on_delete=models.CASCADE)
+    # chat_message = models.ForeignKey(GroupChatMessage, blank=True, null=True, on_delete=models.CASCADE)
     # text_message = models.ForeignKey(, blank=True, null=True)
+    chat_id = models.IntegerField(default=0)  # 如果不为0，即和团队编号相同
+    doc_id = models.IntegerField(default=0)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
 
     def get_content(self):
-        if self.chat_message is not None:
-            content = self.user.username + ' 在' + self.chat_message.group.name + ' 的群聊中@了你'
+        if self.chat_id != 0:
+            content = self.user.username + ' 在' + Team.objects.get(id=self.chat_id).name + ' 的群聊中@了你'
             return content
         else:
-            # 是项目的@
-            pass
+            content = '内容暂时无法显示'
+            return content
 
     def to_dic(self):
         result = {
